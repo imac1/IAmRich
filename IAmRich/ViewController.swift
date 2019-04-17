0f0775ca6b69b1f889626f12e4decfc91fcfd45f
imac1 <Februarie1977>
2019-04-17 21:45:55 +0300

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by MacBook Air on 14/04/2019.
+//  Copyright © 2019 imac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
